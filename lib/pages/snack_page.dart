24fbf15cf3c8890281d981d565b3b86ee323d296
fabien <flierville@cesi.fr>
2024-01-03 13:27:19 +0100

    first

---
+++ lib/pages/snack_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SnackPage extends StatefulWidget {
+  const SnackPage({super.key});
+
+  @override
+  State<SnackPage> createState() => _SnackPageState();
+}
+
+class _SnackPageState extends State<SnackPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
